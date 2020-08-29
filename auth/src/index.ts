c00696dd3014f03aba82748a6fc6574ecdf63d8c
Tatsuya Watanabe <blancnoir.tw@gmail.com>
2020-08-29 15:37:53 +0900

    skaffold routes

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,12 +1,18 @@
 import express from 'express'
 import { json } from 'body-parser'
 
+import { currentUserRouter } from './routes/current-user'
+import { signinRouter } from './routes/signin'
+import { signoutRouter } from './routes/signout'
+import { signup } from './routes/signup'
+
 const app = express()
 app.use(json())
 
-app.get('/api/users/currentuser', (req, res) => {
-  res.send('Hi there!')
-})
+app.use(currentUserRouter)
+app.use(signinRouter)
+app.use(signoutRouter)
+app.use(signup)
 
 app.listen(3000, () => {
   console.log('Listening on port 3000')
