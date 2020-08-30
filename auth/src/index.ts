2cb94032d67947440dc3223ab9b2980963ccc79b
Tatsuya Watanabe <blancnoir.tw@gmail.com>
2020-08-30 14:47:31 +0900

    update error handling

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -5,6 +5,8 @@ import { currentUserRouter } from './routes/current-user'
 import { signinRouter } from './routes/signin'
 import { signoutRouter } from './routes/signout'
 import { signup } from './routes/signup'
+import { errorHandler } from './middlewares/error-handler'
+import { NotFoundError } from './erros/not-found-error'
 
 const app = express()
 app.use(json())
@@ -13,6 +15,12 @@ app.use(currentUserRouter)
 app.use(signinRouter)
 app.use(signoutRouter)
 app.use(signup)
+
+app.all('*', () => {
+  throw new NotFoundError()
+})
+
+app.use(errorHandler)
 
 app.listen(3000, () => {
   console.log('Listening on port 3000')
