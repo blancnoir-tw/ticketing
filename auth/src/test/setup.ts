3abfe90686414f92b1b70040f505ffc9fcd8dc4e
Tatsuya Watanabe <blancnoir.tw@gmail.com>
2020-08-31 12:11:59 +0900

    add helper function

--- auth/src/test/setup.ts
+++ auth/src/test/setup.ts
@@ -1,6 +1,15 @@
 import { MongoMemoryServer } from 'mongodb-memory-server'
 import mongoose from 'mongoose'
+import request from 'supertest'
 import { app } from '../app'
+
+declare global {
+  namespace NodeJS {
+    interface Global {
+      signup: () => Promise<string[]>
+    }
+  }
+}
 
 let mongo: any
 beforeAll(async () => {
@@ -27,3 +36,13 @@ afterAll(async () => {
   await mongo.stop()
   await mongoose.connection.close()
 })
+
+global.signup = async () => {
+  const email = 'test@test.com'
+  const password = 'password'
+
+  const response = await request(app).post('/api/users/signup').send({ email, password }).expect(201)
+  const cookie = response.get('Set-Cookie')
+
+  return cookie
+}
