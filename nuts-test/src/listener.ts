72a12785ff83db46b70b4849502e6cde699d13fc
Tatsuya Watanabe <blancnoir.tw@gmail.com>
2020-09-05 10:44:15 +0900

    create basic listener and publisher

---
+++ nuts-test/src/listener.ts
@@ -0,0 +1,36 @@
+import nats, { Message } from 'node-nats-streaming'
+console.clear()
+
+const stan = nats.connect('ticketing', '123', {
+  url: 'http://localhost:4222',
+})
+
+stan.on('connect', () => {
+  console.log('Listener connected to NATS')
+
+  stan.on('close', () => {
+    console.log('NATS connection closed!')
+    process.exit()
+  })
+
+  const options = stan
+    .subscriptionOptions()
+    .setManualAckMode(true)
+    .setDeliverAllAvailable()
+    .setDurableName('accounting-service')
+
+  const subscription = stan.subscribe('ticket:created', 'queue-group-name', options)
+
+  subscription.on('message', (msg: Message) => {
+    const data = msg.getData()
+
+    if (typeof data === 'string') {
+      console.log(`Recieved event #${msg.getSequence()}, with data: ${data}`)
+    }
+
+    msg.ack()
+  })
+})
+
+process.on('SIGINT', () => stan.close())
+process.on('SIGTERM', () => stan.close())
