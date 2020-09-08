49c4671f37200485eaae7e718fe83cbad4ea3593
Tatsuya Watanabe <blancnoir.tw@gmail.com>
2020-09-09 08:11:36 +0900

    create ticket update publisher

--- tickets/src/routes/update.ts
+++ tickets/src/routes/update.ts
@@ -2,6 +2,8 @@ import express, { Request, Response } from 'express'
 import { body } from 'express-validator'
 import { validateRequest, NotFoundError, requireAuth, NotAuthorizedError } from '@twtickets/common'
 import { Ticket } from '../models/ticket'
+import { TicketUpdatedPublisher } from '../events/publishers/ticket-updated-publisher'
+import { natsWrapper } from '../nats-wrapper'
 
 const router = express.Router()
 
@@ -31,6 +33,13 @@ router.put(
       price,
     })
     await ticket.save()
+
+    new TicketUpdatedPublisher(natsWrapper.client).publish({
+      id: ticket.id,
+      title: ticket.title,
+      price: ticket.price,
+      userId: ticket.userId,
+    })
 
     res.send(ticket)
   }
