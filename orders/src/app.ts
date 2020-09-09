3da5864ffc2820e3a0031fb1aded2022f76308e7
Tatsuya Watanabe <blancnoir.tw@gmail.com>
2020-09-09 09:25:59 +0900

    create basic routes

--- orders/src/app.ts
+++ orders/src/app.ts
@@ -4,6 +4,11 @@ import { json } from 'body-parser'
 import cookieSession from 'cookie-session'
 import { errorHandler, NotFoundError, currentUser } from '@twtickets/common'
 
+import { deleteOrderRouter } from './routes/delete'
+import { indexOrderRouter } from './routes/index'
+import { newOrderRouter } from './routes/new'
+import { showOrderRouter } from './routes/show'
+
 const app = express()
 app.set('trust proxy', true)
 app.use(json())
@@ -14,6 +19,11 @@ app.use(
   })
 )
 app.use(currentUser)
+
+app.use(deleteOrderRouter)
+app.use(indexOrderRouter)
+app.use(newOrderRouter)
+app.use(showOrderRouter)
 
 app.all('*', async (req, res) => {
   throw new NotFoundError()
