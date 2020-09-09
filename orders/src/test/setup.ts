5c17b99d1176069eda3fdb2ac1c36907f574807a
Tatsuya Watanabe <blancnoir.tw@gmail.com>
2020-09-09 16:03:11 +0900

    copy test setup and mocks

---
+++ orders/src/test/setup.ts
@@ -0,0 +1,55 @@
+import { MongoMemoryServer } from 'mongodb-memory-server'
+import mongoose from 'mongoose'
+import jwt from 'jsonwebtoken'
+
+declare global {
+  namespace NodeJS {
+    interface Global {
+      signup: () => string[]
+    }
+  }
+}
+
+jest.mock('../nats-wrapper.ts')
+
+let mongo: any
+beforeAll(async () => {
+  process.env.JWT_KEY = 'asdfasdf'
+
+  mongo = new MongoMemoryServer()
+  const mongoUri = await mongo.getUri()
+
+  await mongoose.connect(mongoUri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+})
+
+beforeEach(async () => {
+  jest.clearAllMocks()
+  const collections = await mongoose.connection.db.collections()
+
+  for (let collection of collections) {
+    await collection.deleteMany({})
+  }
+})
+
+afterAll(async () => {
+  await mongo.stop()
+  await mongoose.connection.close()
+})
+
+global.signup = () => {
+  const id = new mongoose.Types.ObjectId().toHexString()
+
+  const payload = {
+    id,
+    email: 'test@test.com',
+  }
+
+  const token = jwt.sign(payload, process.env.JWT_KEY!) // already check at index.ts
+  const session = { jwt: token }
+  const sessionJSON = JSON.stringify(session)
+  const base64 = Buffer.from(sessionJSON).toString('base64')
+  return [`express:sess=${base64}`]
+}
