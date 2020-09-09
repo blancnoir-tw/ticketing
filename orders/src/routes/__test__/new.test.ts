d9cd25697cbe8081c82663584725877a40e83ecc
Tatsuya Watanabe <blancnoir.tw@gmail.com>
2020-09-09 16:20:20 +0900

    create new test

---
+++ orders/src/routes/__test__/new.test.ts
@@ -0,0 +1,40 @@
+import request from 'supertest'
+import mongoose from 'mongoose'
+import { app } from '../../app'
+import { Order, OrderStatus } from '../../models/order'
+import { Ticket } from '../../models/ticket'
+
+it('returns an error if the ticket does not exist', async () => {
+  const ticketId = mongoose.Types.ObjectId()
+  await request(app).post('/api/orders').set('Cookie', global.signup()).send({ ticketId }).expect(404)
+})
+
+it('return an error if the ticket is already reserved', async () => {
+  const ticket = Ticket.build({
+    title: 'concert',
+    price: 10,
+  })
+  await ticket.save()
+
+  const order = Order.build({
+    ticket,
+    userId: 'asdfasdf',
+    status: OrderStatus.Created,
+    expiresAt: new Date(),
+  })
+  await order.save()
+
+  await request(app).post('/api/orders').set('Cookie', global.signup()).send({ ticketId: ticket.id }).expect(400)
+})
+
+it('returns a ticket', async () => {
+  const ticket = Ticket.build({
+    title: 'concert',
+    price: 10,
+  })
+  await ticket.save()
+
+  await request(app).post('/api/orders').set('Cookie', global.signup()).send({ ticketId: ticket.id }).expect(201)
+})
+
+it.todo('emits an order created event')
