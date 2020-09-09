01b116b2a760b0a622399d90b21c2e3e6123bf22
Tatsuya Watanabe <blancnoir.tw@gmail.com>
2020-09-09 09:02:11 +0900

    define nats env variables

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -1,5 +1,4 @@
 import mongoose from 'mongoose'
-import { randomBytes } from 'crypto'
 
 import { app } from './app'
 import { natsWrapper } from './nats-wrapper'
@@ -11,9 +10,18 @@ const start = async () => {
   if (!process.env.MONGO_URI) {
     throw new Error('MONGO_URI must be defined')
   }
+  if (!process.env.NATS_CLIENT_ID) {
+    throw new Error('NATS_CLIENT_ID must be defined')
+  }
+  if (!process.env.NATS_URL) {
+    throw new Error('NATS_URL must be defined')
+  }
+  if (!process.env.NATS_CLUSTER_ID) {
+    throw new Error('NATS_CLUSTER_ID must be defined')
+  }
 
   try {
-    await natsWrapper.connect('ticketing', randomBytes(4).toString('hex'), 'http://nats-srv:4222')
+    await natsWrapper.connect(process.env.NATS_CLUSTER_ID, process.env.NATS_CLIENT_ID, process.env.NATS_URL)
     natsWrapper.client.on('close', () => {
       console.log('NATS connection closed!')
       process.exit()
