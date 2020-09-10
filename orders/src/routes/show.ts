1c7b39f3e81c4fdd6f2e1206593667341ee21f95
Tatsuya Watanabe <blancnoir.tw@gmail.com>
2020-09-10 17:25:47 +0900

    create show route

--- orders/src/routes/show.ts
+++ orders/src/routes/show.ts
@@ -1,9 +1,21 @@
 import express, { Request, Response } from 'express'
+import { requireAuth, NotFoundError, NotAuthorizedError } from '@twtickets/common'
+import { Order } from '../models/order'
 
 const router = express.Router()
 
-router.get('/api/orders/:orderId', async (req: Request, res: Response) => {
-  res.send({})
+router.get('/api/orders/:orderId', requireAuth, async (req: Request, res: Response) => {
+  const order = await Order.findById(req.params.orderId).populate('ticket')
+  if (!order) {
+    throw new NotFoundError()
+  }
+
+  // already check requireAuth
+  if (order.userId !== req.currentUser!.id) {
+    throw new NotAuthorizedError()
+  }
+
+  res.send(order)
 })
 
 export { router as showOrderRouter }
