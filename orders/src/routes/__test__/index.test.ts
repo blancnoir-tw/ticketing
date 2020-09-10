acc4896b8baa868082579f716e2be667ad035d21
Tatsuya Watanabe <blancnoir.tw@gmail.com>
2020-09-10 17:13:52 +0900

    create index test

---
+++ orders/src/routes/__test__/index.test.ts
@@ -0,0 +1,42 @@
+import request from 'supertest'
+import { app } from '../../app'
+import { Order } from '../../models/order'
+import { Ticket } from '../../models/ticket'
+
+const buildTicket = async () => {
+  const ticket = Ticket.build({
+    title: 'concert',
+    price: 10,
+  })
+  await ticket.save()
+
+  return ticket
+}
+
+it('fetches orders for an particular user', async () => {
+  const ticket1 = await buildTicket()
+  const ticket2 = await buildTicket()
+  const ticket3 = await buildTicket()
+
+  const user1 = global.signup()
+  const user2 = global.signup()
+
+  await request(app).post('/api/orders').set('Cookie', user1).send({ ticketId: ticket1.id }).expect(201)
+  const { body: order2 } = await request(app)
+    .post('/api/orders')
+    .set('Cookie', user2)
+    .send({ ticketId: ticket2.id })
+    .expect(201)
+  const { body: order3 } = await request(app)
+    .post('/api/orders')
+    .set('Cookie', user2)
+    .send({ ticketId: ticket3.id })
+    .expect(201)
+
+  const response = await request(app).get('/api/orders').set('Cookie', user2).expect(200)
+  expect(response.body.length).toEqual(2)
+  expect(response.body[0].id).toEqual(order2.id)
+  expect(response.body[1].id).toEqual(order3.id)
+  expect(response.body[0].ticket.id).toEqual(ticket2.id)
+  expect(response.body[1].ticket.id).toEqual(ticket3.id)
+})
