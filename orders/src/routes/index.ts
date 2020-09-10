6377d374003c13c7c4709bd15678aaae4b3d2be6
Tatsuya Watanabe <blancnoir.tw@gmail.com>
2020-09-10 17:14:15 +0900

    create index route

--- orders/src/routes/index.ts
+++ orders/src/routes/index.ts
@@ -1,9 +1,15 @@
 import express, { Request, Response } from 'express'
+import { requireAuth } from '@twtickets/common'
+import { Order } from '../models/order'
 
 const router = express.Router()
 
-router.get('/api/orders', async (req: Request, res: Response) => {
-  res.send({})
+router.get('/api/orders', requireAuth, async (req: Request, res: Response) => {
+  const orders = await Order.find({
+    userId: req.currentUser!.id, // already check requireAuth
+  }).populate('ticket')
+
+  res.send(orders)
 })
 
 export { router as indexOrderRouter }
