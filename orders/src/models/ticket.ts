d7eb7514b883b6851c809a8070f487bc0cf30f77
Tatsuya Watanabe <blancnoir.tw@gmail.com>
2020-09-12 15:59:03 +0900

    create ticket update listener

--- orders/src/models/ticket.ts
+++ orders/src/models/ticket.ts
@@ -2,6 +2,7 @@ import mongoose from 'mongoose'
 import { Order, OrderStatus } from './order'
 
 interface TicketAttrs {
+  id: string
   title: string
   price: number
 }
@@ -38,8 +39,12 @@ const ticketSchema = new mongoose.Schema(
   }
 )
 
-ticketSchema.statics.build = (attr: TicketAttrs) => {
-  return new Ticket(attr)
+ticketSchema.statics.build = (attrs: TicketAttrs) => {
+  return new Ticket({
+    _id: attrs.id,
+    title: attrs.title,
+    price: attrs.price,
+  })
 }
 ticketSchema.methods.isReserved = async function () {
   const existingOrder = await Order.findOne({
