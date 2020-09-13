898790a66a52ef4f585841b5e1735c226f6645db
Tatsuya Watanabe <blancnoir.tw@gmail.com>
2020-09-13 16:02:54 +0900

    create ticket create listener test

---
+++ orders/src/events/listeners/__test__/ticket-created-listener.test.ts
@@ -0,0 +1,41 @@
+import mongoose from 'mongoose'
+import { Message } from 'node-nats-streaming'
+import { TicketCreatedEvent } from '@twtickets/common'
+import { TicketCreatedListener } from '../ticket-created-listener'
+import { natsWrapper } from '../../../nats-wrapper'
+import { Ticket } from '../../../models/ticket'
+
+const setup = () => {
+  const listener = new TicketCreatedListener(natsWrapper.client)
+  const data: TicketCreatedEvent['data'] = {
+    version: 0,
+    id: new mongoose.Types.ObjectId().toHexString(),
+    title: 'conert',
+    price: 10,
+    userId: new mongoose.Types.ObjectId().toHexString(),
+  }
+  // @ts-ignore
+  const msg: Message = {
+    ack: jest.fn(),
+  }
+
+  return { listener, data, msg }
+}
+
+it('creates and saves a ticket', async () => {
+  const { listener, data, msg } = setup()
+  await listener.onMessage(data, msg)
+
+  const ticket = await Ticket.findById(data.id)
+
+  expect(ticket).toBeDefined()
+  expect(ticket!.title).toEqual(data.title)
+  expect(ticket!.price).toEqual(data.price)
+})
+
+it('acks the message', async () => {
+  const { listener, data, msg } = setup()
+  await listener.onMessage(data, msg)
+
+  expect(msg.ack).toHaveBeenCalled()
+})
