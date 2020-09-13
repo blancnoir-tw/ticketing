cd487a6127fb900df958ef7cad5094229911c385
Tatsuya Watanabe <blancnoir.tw@gmail.com>
2020-09-13 14:32:58 +0900

    add version key and create test

---
+++ tickets/src/models/__test__/ticket.test.ts
@@ -0,0 +1,40 @@
+import { Ticket } from '../ticket'
+
+it('implements optimistic concurrency control', async done => {
+  const ticket = Ticket.build({
+    title: 'concert',
+    price: 10,
+    userId: '123',
+  })
+  await ticket.save()
+
+  const firstInstance = await Ticket.findById(ticket.id)
+  const secondInstance = await Ticket.findById(ticket.id)
+
+  firstInstance!.set({ price: 20 })
+  secondInstance!.set({ price: 30 })
+
+  await firstInstance!.save()
+
+  try {
+    await secondInstance!.save()
+  } catch (err) {
+    return done()
+  }
+
+  throw new Error('Should not reach this point')
+})
+
+it('increments the version number on multiple saves', async () => {
+  const ticket = Ticket.build({
+    title: 'concert',
+    price: 10,
+    userId: '123',
+  })
+  await ticket.save()
+  expect(ticket.version).toEqual(0)
+  await ticket!.save()
+  expect(ticket.version).toEqual(1)
+  await ticket!.save()
+  expect(ticket.version).toEqual(2)
+})
