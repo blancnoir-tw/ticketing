416e9b7766b42bc15ce5ebb1de1ee58ae0b20b08
Tatsuya Watanabe <blancnoir.tw@gmail.com>
2020-09-17 07:47:31 +0900

    create order-created-listener

---
+++ payments/src/events/listeners/__test__/order-created-listener.test.ts
@@ -0,0 +1,44 @@
+import mongoose from 'mongoose'
+import { OrderCreatedEvent, OrderStatus } from '@twtickets/common'
+import { natsWrapper } from '../../../nats-wrapper'
+import { OrderCreatedListener } from '../order-created-listener'
+import { Message } from 'node-nats-streaming'
+import { Order } from '../../../models/order'
+
+const setup = () => {
+  const listener = new OrderCreatedListener(natsWrapper.client)
+
+  const data: OrderCreatedEvent['data'] = {
+    id: mongoose.Types.ObjectId().toHexString(),
+    version: 0,
+    expiresAt: 'asdf',
+    userId: 'asdf',
+    status: OrderStatus.Created,
+    ticket: {
+      id: 'asdf',
+      price: 10,
+    },
+  }
+
+  // @ts-ignore
+  const msg: Message = {
+    ack: jest.fn(),
+  }
+
+  return { listener, data, msg }
+}
+
+it('replicates the order info', async () => {
+  const { listener, data, msg } = setup()
+  await listener.onMessage(data, msg)
+
+  const order = await Order.findById(data.id)
+  expect(order!.price).toEqual(data.ticket.price)
+})
+
+it('acks', async () => {
+  const { listener, data, msg } = setup()
+  await listener.onMessage(data, msg)
+
+  expect(msg.ack).toHaveBeenCalled()
+})
