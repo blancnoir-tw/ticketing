8fd980b2a937acf4180ff13b2658b8c4a6a99c87
Tatsuya Watanabe <blancnoir.tw@gmail.com>
2020-09-16 14:30:32 +0900

    create expiration-complite-listener

---
+++ orders/src/events/listeners/__test__/expiration-compolete-listener.test.ts
@@ -0,0 +1,62 @@
+import mongoose from 'mongoose'
+import { natsWrapper } from '../../../nats-wrapper'
+import { ExpirationCompleteListener } from '../expiration-complete-listener'
+import { Order, OrderStatus } from '../../../models/order'
+import { Ticket } from '../../../models/ticket'
+import { ExpirationCompleteEvent } from '@twtickets/common'
+import { Message } from 'node-nats-streaming'
+
+const setup = async () => {
+  const listener = new ExpirationCompleteListener(natsWrapper.client)
+
+  const ticket = Ticket.build({
+    id: mongoose.Types.ObjectId().toHexString(),
+    title: 'concert',
+    price: 10,
+  })
+  await ticket.save()
+
+  const order = Order.build({
+    status: OrderStatus.Created,
+    userId: 'asdf',
+    expiresAt: new Date(),
+    ticket,
+  })
+  await order.save()
+
+  const data: ExpirationCompleteEvent['data'] = {
+    orderId: order.id,
+  }
+
+  // @ts-ignore
+  const msg: Message = {
+    ack: jest.fn(),
+  }
+
+  return { listener, order, ticket, data, msg }
+}
+
+it('updates the order status to cancelled', async () => {
+  const { listener, order, data, msg } = await setup()
+  await listener.onMessage(data, msg)
+
+  const updatedOrder = await Order.findById(order.id)
+  expect(updatedOrder!.status).toEqual(OrderStatus.Cancelled)
+})
+
+it('emit an OrderCancelled event', async () => {
+  const { listener, order, data, msg } = await setup()
+  await listener.onMessage(data, msg)
+
+  expect(natsWrapper.client.publish).toHaveBeenCalled()
+
+  const eventData = JSON.parse((natsWrapper.client.publish as jest.Mock).mock.calls[0][1])
+  expect(eventData.id).toEqual(order.id)
+})
+
+it('ack the message', async () => {
+  const { listener, data, msg } = await setup()
+  await listener.onMessage(data, msg)
+
+  expect(msg.ack).toHaveBeenCalled()
+})
---
+++ orders/src/events/listeners/expiration-complete-listener.ts
@@ -0,0 +1,31 @@
+import { Message } from 'node-nats-streaming'
+import { Listener, Subjects, ExpirationCompleteEvent, OrderStatus } from '@twtickets/common'
+import { queueGroupName } from './queue-group-name'
+import { Order } from '../../models/order'
+import { OrderCanceledPublisher } from '../publisher/order-cancelled-publisher'
+
+export class ExpirationCompleteListener extends Listener<ExpirationCompleteEvent> {
+  readonly subject = Subjects.ExpirationComplete
+  queueGroupName = queueGroupName
+
+  async onMessage(data: ExpirationCompleteEvent['data'], msg: Message) {
+    const order = await Order.findById(data.orderId).populate('ticket')
+    if (!order) {
+      throw new Error('Order not found')
+    }
+    order.set({
+      status: OrderStatus.Cancelled,
+    })
+    await order.save()
+
+    await new OrderCanceledPublisher(this.client).publish({
+      id: order.id,
+      version: order.version,
+      ticket: {
+        id: order.ticket.id,
+      },
+    })
+
+    msg.ack()
+  }
+}
