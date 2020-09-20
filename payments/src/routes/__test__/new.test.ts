ada70d8d79ff1e990e61aa64632ecb2005e64013
Tatsuya Watanabe <blancnoir.tw@gmail.com>
2020-09-20 13:37:44 +0900

    use stripe

--- payments/src/routes/__test__/new.test.ts
+++ payments/src/routes/__test__/new.test.ts
@@ -3,7 +3,10 @@ import mongoose from 'mongoose'
 import request from 'supertest'
 import { app } from '../../app'
 import { Order } from '../../models/order'
+import { stripe } from '../../stripe'
 
+jest.mock('../../stripe')
+const stripeToken = 'tok_visa' // stripe test token
 const path = '/api/payments'
 
 it('returns a 404 when purchasing an order that does not exist', async () => {
@@ -44,4 +47,28 @@ it('returns a 400 when purchasing a cancelled order', async () => {
     .set('Cookie', global.signup(userId))
     .send({ token: 'asdf', orderId: order.id })
     .expect(400)
+})
+
+it('returns a 204 with valid inputs', async () => {
+  const userId = mongoose.Types.ObjectId().toHexString()
+
+  const order = Order.build({
+    id: mongoose.Types.ObjectId().toHexString(),
+    userId,
+    version: 0,
+    price: 1000,
+    status: OrderStatus.Created,
+  })
+  await order.save()
+
+  await request(app)
+    .post(path)
+    .set('Cookie', global.signup(userId))
+    .send({ token: stripeToken, orderId: order.id })
+    .expect(201)
+
+  const chargeOptions = (stripe.charges.create as jest.Mock).mock.calls[0][0]
+  expect(chargeOptions.source).toEqual(stripeToken)
+  expect(chargeOptions.amount).toEqual(1000)
+  expect(chargeOptions.currency).toEqual('jpy')
 })
